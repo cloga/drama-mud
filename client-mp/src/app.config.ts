aabe9e307b5a02406a5fb4f3632c5206f383e2e7
Cloga Chen <cloga0216@gmail.com>
2026-04-06 23:03:57 +0800

    import: client-mp/src/app.config.ts

---
+++ client-mp/src/app.config.ts
@@ -0,0 +1,13 @@
+export default {
+  pages: [
+    'pages/lobby/index',
+    'pages/character-select/index',
+    'pages/chat/index',
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#1a1a2e',
+    navigationBarTitleText: 'Drama MUD',
+    navigationBarTextStyle: 'white',
+  },
+}
